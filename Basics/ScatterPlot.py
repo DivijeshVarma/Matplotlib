369036a4de2694a058b03c936c1548c396cf6c69
divi <divijeshvarma@gmail.com>
2021-02-02 12:15:51 +0530

    Plotting a Series of Points with scatter and Calculating Data Automatically

--- Basics/ScatterPlot.py
+++ Basics/ScatterPlot.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # set chart title and label axis
 ax.set_title('Square Numbers', fontsize=24)
@@ -14,5 +14,11 @@ ax.set_ylabel('Square value', fontsize=14)
 
 # set size of tick labels
 ax.tick_params(axis='both', which='major', labelsize=14)
+# If you want to disable both the offset and scientific notation.
+ax.ticklabel_format(useOffset=False, style='plain')
+
+# set range of each axis
+ax.axis([0, 1100, 0, 1100000])
+plt.tight_layout()
 
 plt.show()
