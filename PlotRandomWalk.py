8b97bdd43fc289f7a23bd596e2838d31ca9a16b9
divi <divijeshvarma@gmail.com>
2021-02-03 13:46:06 +0530

    Generating Multiple Random Walks using while loop.

--- PlotRandomWalk.py
+++ PlotRandomWalk.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 from RandomWalk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
 
-plt.show()
+    plt.show()
+
+    response = input('continue Random walk y/n: ')
+    if response == 'n':
+        break
