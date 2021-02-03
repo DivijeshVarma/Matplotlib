4a9e5f9ee8dbccbc10b4170763c8191d1b6a0fab
divi <divijeshvarma@gmail.com>
2021-02-03 14:32:02 +0530

    Altering the Size to Fill the Screen.

--- PlotRandomWalk.py
+++ PlotRandomWalk.py
@@ -8,7 +8,7 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots(figsize=(18, 9))
+    fig, ax = plt.subplots(figsize=(18, 9), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=5)
     # Emphasize the first and last points.
