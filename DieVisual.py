9b8d0314a01f5fa7224e15974940a72168653756
divi <divijeshvarma@gmail.com>
2021-02-04 13:01:39 +0530

    analyze the results of rolling one D6

--- DieVisual.py
+++ DieVisual.py
@@ -5,8 +5,17 @@ die = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(50):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# print(results)
+
+# We’ll analyze the results of rolling one D6 by counting how many times we roll each number:
+frequencies = []
+for value in range(1, die.sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
