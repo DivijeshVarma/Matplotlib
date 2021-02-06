2d8b6fc5d3c1632e32ff00e09c717fcc52811d3b
divi <divijeshvarma@gmail.com>
2021-02-06 12:34:35 +0530

    Plotting dates using datetime

--- Downloading-Data/CSVFileHeaders.py
+++ Downloading-Data/CSVFileHeaders.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 file = 'Data/sitka_weather_07-2018_simple.csv'
 with open(file) as f:
@@ -7,15 +8,17 @@ with open(file) as f:
     header_row = next(reader)
     # print(header_row)
 
-    # The enumerate() function returns both the index of each item and the value of each item as you loop through a list.
+    # The enumerate() function returns both the index of each item and the value of each item as you loop through a list
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
     # getting high temp from file
-    highs = []
+    dates, highs = [], []
 
     for data in reader:
+        current_date = datetime.strptime(data[2], '%Y-%m-%d')
         high = int(data[5])
+        dates.append(current_date)
         highs.append(high)
 
 print(highs)
@@ -23,12 +26,14 @@ print(highs)
 # plot High temp
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Format plot
 plt.title("Daily high temperatures, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel('Temperature (F)', fontsize=16)
+fig.autofmt_xdate()
+# plt.tight_layout()
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
