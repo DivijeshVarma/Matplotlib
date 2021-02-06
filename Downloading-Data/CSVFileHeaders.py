edcf25fe4a19b0b97995875a66475eab6cf7cf78
divi <divijeshvarma@gmail.com>
2021-02-06 11:17:11 +0530

    Plotting max temps using pyplot

--- Downloading-Data/CSVFileHeaders.py
+++ Downloading-Data/CSVFileHeaders.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 file = 'Data/sitka_weather_07-2018_simple.csv'
 with open(file) as f:
@@ -6,9 +7,9 @@ with open(file) as f:
     header_row = next(reader)
     # print(header_row)
 
-# The enumerate() function returns both the index of each item and the value of each item as you loop through a list.
-# for index, column_header in enumerate(header_row):
-#     print(index, column_header)
+    # The enumerate() function returns both the index of each item and the value of each item as you loop through a list.
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
 
     # getting high temp from file
     highs = []
@@ -18,3 +19,16 @@ with open(file) as f:
         highs.append(high)
 
 print(highs)
+
+# plot High temp
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
