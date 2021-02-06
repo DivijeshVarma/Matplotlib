e04296c2fd22bad4cbe5a73ff9de9d4314a5786d
divi <divijeshvarma@gmail.com>
2021-02-06 10:38:12 +0530

    Printing the Headers and Their Positions

--- Downloading-Data/CSVFileHeaders.py
+++ Downloading-Data/CSVFileHeaders.py
@@ -4,4 +4,8 @@ file = 'Data/sitka_weather_07-2018_simple.csv'
 with open(file) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    # print(header_row)
+
+# The enumerate() function returns both the index of each item and the value of each item as you loop through a list.
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
