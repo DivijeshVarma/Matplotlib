5af992550a6df0b53b04bc62e609488eed860f2a
divi <divijeshvarma@gmail.com>
2021-02-08 10:45:46 +0530

    plotting temps without errors

---
+++ Downloading-Data/CSVFileErroes.py
@@ -0,0 +1,51 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+file = 'Data/death_valley_2018_simple.csv'
+with open(file) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    # print(header_row)
+
+    # The enumerate() function returns both the index of each item and the value of each item as you loop through a list
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # getting high temp from file
+    dates, highs, lows = [], [], []
+
+    for data in reader:
+        current_date = datetime.strptime(data[2], '%Y-%m-%d')
+        try:
+            high = int(data[4])
+            low = int(data[5])
+        except ValueError:
+            print(f'{current_date} Data missing')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# print(highs)
+# print(lows)
+# print(dates)
+
+# plot High temp
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+# Shading an Area in the Chart
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot
+plt.title("Daily high & low temperatures death valley- 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+fig.autofmt_xdate()
+# plt.tight_layout()
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
