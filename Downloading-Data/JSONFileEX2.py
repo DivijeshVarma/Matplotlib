8294334491813d1ee6fdada2022a00e29d79f88f
divi <divijeshvarma@gmail.com>
2021-02-09 10:50:48 +0530

    mapping World Fires.

--- Downloading-Data/JSONFileEX2.py
+++ Downloading-Data/JSONFileEX2.py
@@ -8,58 +8,57 @@ import csv
 import json
 from plotly.graph_objs import Scattergeo, Layout
 from datetime import datetime
-from matplotlib import pyplot as plt
 from plotly import offline
-#
-# # Let’s start by loading the data and displaying it in a format that’s easier to read.
-# # This is a long data file, so instead of printing it, we’ll rewrite the data to a new file.
-# # Then we can open that file and scroll back and forth eas- ily through the data:
-#
-# file = 'Data/significant_month_data.json'
-# with open(file) as f:
-#     all_eq_data = json.load(f)
-#
-# # Making a List of All Earthquakes
-# # readable_file = 'Data/readablesignificant_month_data.json'
-# # with open(readable_file, 'w') as f:
-# #     json.dump(all_eq_data, f, indent=4)
-#
-# all_eq_dicts = all_eq_data['features']
-# # print(len(all_eq_dicts))
-#
-# # Extracting Magnitudes
-# # Extracting Location Data
-# mags, lons, lats, hover_texts = [], [], [], []
-# for eq_dict in all_eq_dicts:
-#     mags.append(eq_dict['properties']['mag'])
-#     lons.append(eq_dict['geometry']['coordinates'][0])
-#     lats.append(eq_dict['geometry']['coordinates'][1])
-#     hover_texts.append(eq_dict['properties']['title'])
-#
-# # print(mags[:10])
-# # print(lons[:10])
-# # print(lats[:10])
-#
-# # # Map the earthquakes.
-# # data = [Scattergeo(lon=lons, lat=lats)]
-# data = [{
-#     'type': 'scattergeo',
-#     'lon': lons,
-#     'lat': lats,
-#     'text': hover_texts,
-#     'marker': {
-#         'size': [5*mag for mag in mags],
-#         'color': mags,
-#         'colorscale': 'Viridis',
-#         'reversescale': True,
-#         'colorbar': {'title': 'Magnitude'},
-#     }
-# }]
-# title = all_eq_data['metadata']['title']
-# my_layout = Layout(title=title)
-#
-# fig = {'data': data, 'layout': my_layout}
-# offline.plot(fig, filename='global_earthquakes.html')
+
+# Let’s start by loading the data and displaying it in a format that’s easier to read.
+# This is a long data file, so instead of printing it, we’ll rewrite the data to a new file.
+# Then we can open that file and scroll back and forth eas- ily through the data:
+
+file = 'Data/significant_month_data.json'
+with open(file) as f:
+    all_eq_data = json.load(f)
+
+# Making a List of All Earthquakes
+# readable_file = 'Data/readablesignificant_month_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+# Extracting Magnitudes
+# Extracting Location Data
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
+
+# print(mags[:10])
+# print(lons[:10])
+# print(lats[:10])
+
+# # Map the earthquakes.
+# data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    }
+}]
+title = all_eq_data['metadata']['title']
+my_layout = Layout(title=title)
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
 
 # 16-9. World Fires: In the resources for this chapter, you’ll find a file called world_fires_1_day.csv.
 # This file contains information about fires burning in different locations around the globe,
