c201c626b6b14df56f5f7c9cb707497e6d780f97
divi <divijeshvarma@gmail.com>
2021-02-09 09:44:51 +0530

    To finish this map, we’ll add some informative text that appears when you hover over the marker representing an earthquake..

--- Downloading-Data/JSONFileQuakes.py
+++ Downloading-Data/JSONFileQuakes.py
@@ -25,11 +25,11 @@ for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
-    hover_text = eq_dict['properties']['title']
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
-    hover_texts.append(hover_text)
+    hover_texts.append(title)
 
 # print(mags[:10])
 # print(lons[:10])
