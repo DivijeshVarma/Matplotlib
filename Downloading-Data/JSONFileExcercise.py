f9946985bfe7ae7e8da8337b62bd0de8c4402df5
divi <divijeshvarma@gmail.com>
2021-02-09 10:12:21 +0530

    shorten the body of this loop and Automate Title

--- Downloading-Data/JSONFileExcercise.py
+++ Downloading-Data/JSONFileExcercise.py
@@ -12,50 +12,50 @@ from plotly import offline
 # This is a long data file, so instead of printing it, we’ll rewrite the data to a new file.
 # Then we can open that file and scroll back and forth eas- ily through the data:
 
-# file = 'Data/eq_data_30_day_m1.json'
-# with open(file) as f:
-#     all_eq_data = json.load(f)
-#
-# # Making a List of All Earthquakes
-# # readable_file = 'Data/readable_eq_data.json'
-# # with open(readable_file, 'w') as f:
-# #     json.dump(all_eq_data, f, indent=4)
-#
-# all_eq_dicts = all_eq_data['features']
-# # print(len(all_eq_dicts))
-#
-# # Extracting Magnitudes
-# # Extracting Location Data
-# mags, lons, lats, hover_texts = [], [], [], []
-# for eq_dict in all_eq_dicts:
-#     mags.append(eq_dict['properties']['mag'])
-#     lons.append(eq_dict['geometry']['coordinates'][0])
-#     lats.append(eq_dict['geometry']['coordinates'][1])
-#     hover_texts.append(eq_dict['properties']['title'])
-#
-# # print(mags[:10])
-# # print(lons[:10])
-# # print(lats[:10])
-#
-# # # Map the earthquakes.
-# # data = [Scattergeo(lon=lons, lat=lats)]
-# data = [{
-#     'type': 'scattergeo',
-#     'lon': lons,
-#     'lat': lats,
-#     'text': hover_texts,
-#     'marker': {
-#         'size': [5*mag for mag in mags],
-#         'color': mags,
-#         'colorscale': 'Viridis',
-#         'reversescale': True,
-#         'colorbar': {'title': 'Magnitude'},
-#     }
-# }]
-# my_layout = Layout(title='Global Earthquakes')
-#
-# fig = {'data': data, 'layout': my_layout}
-# offline.plot(fig, filename='global_earthquakes.html')
+file = 'Data/eq_data_30_day_m1.json'
+with open(file) as f:
+    all_eq_data = json.load(f)
+
+# Making a List of All Earthquakes
+# readable_file = 'Data/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+# Extracting Magnitudes
+# Extracting Location Data
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
+
+# print(mags[:10])
+# print(lons[:10])
+# print(lats[:10])
+
+# # Map the earthquakes.
+# data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    }
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
 
 # 16-7. Automated Title: In this section, we specified the title manually when defining my_layout,
 # which means we have to remember to update the title every time the source file changes.
@@ -107,3 +107,4 @@ my_layout = Layout(title=layout_title)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
+
