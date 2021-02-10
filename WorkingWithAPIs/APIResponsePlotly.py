cd78a1ba5cd9e678e4052cfbc1f42acdfe1cc7c5
divi <divijeshvarma@gmail.com>
2021-02-10 10:06:23 +0530

    Adding Custom Tooltips for hovering

--- WorkingWithAPIs/APIResponsePlotly.py
+++ WorkingWithAPIs/APIResponsePlotly.py
@@ -11,16 +11,24 @@ print(f'status code: {r.status_code}')
 # Process Results
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    # In Plotly, you can hover the cursor over an individual bar to show the information that the bar represents.
+    # This is commonly called a tooltip.
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
 
 # make visualization
 data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'hovertext': labels,
     'marker': {'color': 'rgb(60, 100, 150)',
                'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
                },
